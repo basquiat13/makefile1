0ead2da93182f53f905af51bf7bbde279355062c
lailah smith <117845543+basquiat13@users.noreply.github.com>
2025-01-22 18:13:50 -0500

    Update date.cpp

--- date.cpp
+++ date.cpp
@@ -1,3 +1,37 @@
 #include "date.h"
 #include <iostream>
 
+Date::Date() : day(16), month(1), year(2025){}
+
+void Date::setDay(int d) {
+  day= d;
+}
+
+void Date::setMonth(int m) {
+  month= m;
+}
+
+void Date::setYear(int y) {
+  year= y;
+}
+
+int Date::getDay() const {
+  return day;
+}
+
+int Date::getMonth() const {
+  return month;
+}
+
+int Date::getYear() const {
+  return year;
+}
+
+void Date::displayAmerican() const {
+  ;
+}
+
+void Date::displayEuropean() const {
+  day= d;
+}
+
