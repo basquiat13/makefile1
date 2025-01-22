8a58a1b39009f7b9e9d21167fdf51bc7098da9f1
lailah smith <117845543+basquiat13@users.noreply.github.com>
2025-01-22 18:24:18 -0500

    Update date.h

--- date.h
+++ date.h
@@ -1,1 +1,29 @@
+#ifndef DATE_H
+#define DATE_H
 
+#include <string>
+#include <iostream>
+
+class Date{
+private:
+  int day;
+  int month;
+  int year;
+
+public:
+
+  Date();
+
+void setDay(int d);
+void setMonth(int m);
+void setYear(int y);
+
+int getDay() const;
+int getMonth() const;
+int getYear() const;
+
+void displayAmerican() const;
+void displayEuropean() const;
+};
+
+#endif
